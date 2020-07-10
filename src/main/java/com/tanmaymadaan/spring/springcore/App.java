8748c19f66355d38174d3ae7c026781efa04fdeb
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-10 23:37:00 +0530

    Init Maven project

---
+++ src/main/java/com/tanmaymadaan/spring/springcore/App.java
@@ -0,0 +1,13 @@
+package com.tanmaymadaan.spring.springcore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
